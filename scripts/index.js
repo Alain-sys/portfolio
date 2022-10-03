431638753132495acb0e2a362148e3fb9aa74a66
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2022-10-03 22:57:10 +0200

    fix: languages

--- scripts/index.js
+++ scripts/index.js
@@ -22,18 +22,6 @@ window.onscroll = function () {
   prevScrollPos = currentScrollPos;
 };
 
-showMore.addEventListener('click', () => {
-  showMore.classList.toggle('active');
-
-  if (showMore.classList.contains('active')) {
-    showMore.textContent = 'Show less';
-    secondaryBlockCard.classList.add('active');
-  } else {
-    showMore.textContent = 'Show more';
-    secondaryBlockCard.classList.remove('active');
-  }
-});
-
 const getAPI = async () => {
   const response = await fetch(`scripts/languages.json`);
   let data = await response.json();
@@ -60,6 +48,20 @@ const dataUser = (data) => {
 
   const language = data[index];
 
+  showMore.textContent = language.show_more;
+
+  showMore.addEventListener('click', () => {
+    showMore.classList.toggle('active');
+
+    if (showMore.classList.contains('active')) {
+      showMore.textContent = language.show_less;
+      secondaryBlockCard.classList.add('active');
+    } else {
+      showMore.textContent = language.show_more;
+      secondaryBlockCard.classList.remove('active');
+    }
+  });
+
   document.querySelector('.list__link-home').textContent = language.nav.home;
   document.querySelector('.list__link-about').textContent = language.nav.about;
   document.querySelector('.list__link-work').textContent = language.nav.work;
@@ -82,4 +84,28 @@ const dataUser = (data) => {
   document.querySelector('.card__text-officelite').textContent = language.projects.officelite;
   document.querySelector('.card__text-tip-calculator').textContent = language.projects.tip_calculator;
   document.querySelector('.card__text-maker').textContent = language.projects.maker;
+  document.querySelector('.contact-text').textContent = language.contact_text;
+
+  const cardTextGithub = document.querySelector('.card__text-github-user');
+  const cardTextTipCalculator = document.querySelector('.card__text-tip-calculator');
+
+  if (userLangFirstLetters === 'fr') {
+    cardTextGithub.innerHTML = `Chercher un utilisateur github et l'app se chargera d'afficher ses informations de profil. Dans ce projet j'utilise la 
+    <a class="card__text-link" href="https://docs.github.com/en/rest/users/users#get-a-user" target="_blank">Github users API</a> 
+    pour accèder aux données et les afficher.`;
+
+    cardTextTipCalculator.innerHTML = `Ce projet a été réalisé en pair programming avec 
+    <a class="card__text-link" href="https://github.com/Vcna-0" target="_blank">Noëmie</a>. 
+    Il aide à déterminer de combien sera le pourboire après un bon repas. Choissisez un pourcentage (5%, 10%...), renseignez combien étiez-vous à manger ainsi que la somme total du repas et ça vous donnera le montant total du pourboire divisé par le nombre de personne. Ainsi que la somme total du pourboire.`;
+  } else {
+    cardTextGithub.innerHTML = `Search a github user and the app will display his profile and his details. In this project I used the
+   <a class="card__text-link" href="https://docs.github.com/en/rest/users/users#get-a-user" target="_blank">Github users API</a> 
+   to access and to display profile datas.`;
+
+    cardTextTipCalculator.innerHTML = `This project was realised in pair programming with
+                <a class="card__text-link" href="https://github.com/Vcna-0" target="_blank">Noëmie</a>. 
+                It helps you figure out how much you should
+                tip the waitress after a good meal. Choose a percentage (5%, 10%...), write how many of you were eating and it gives you the amount to
+                tip and the total amount per person.`;
+  }
 };
